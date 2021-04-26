e193949451dd8fbd1ea720e1ff2c4c2e69c3741d
zhoushuren <zhoujun247@foxmail.com>
2021-04-26 17:55:35 +0800

    update

--- contracts/IProduct.sol
+++ contracts/IProduct.sol
@@ -4,5 +4,6 @@ pragma solidity ^0.7.6;
 interface IProduct {
      function deposit(address from, uint256 amount) external;
      function setRate(uint256 _rate) external;
-     function initialize(address _token,  uint256 _rate, uint256 _depositEndTime , address _cashbox ) external;
+     function setRewardCHNG(uint256 _reward)external;
+     function initialize(address _token,  uint256 _rate, uint256 _depositEndTime , address _cashbox, uint256 _rewardCHNG ) external;
 }
--- contracts/Product.sol
+++ contracts/Product.sol
@@ -17,8 +17,13 @@ contract Product is IProduct {
 
   address public factory;
 
+  uint256 public rewardCHNG;
+
+  address public owner;
+
   bytes4 private constant SELECTOR = bytes4(keccak256(bytes('timeSliceTransferFrom(address,address,uint256,uint256,uint256)')));
 
+  bytes4 private constant SELECTOR1 = bytes4(keccak256(bytes('transferFrom(address,address,uint256)')));
 	uint256 public constant MAX_TIME = 18446744073709551615;
 
   mapping (address=> uint256) balanceOf;
@@ -27,15 +32,23 @@ contract Product is IProduct {
       factory = msg.sender;
   }
 
-  function initialize(address _token,  uint256 _rate, uint256 _depositEndTime , address _cashbox ) public override {
+  function initialize(address _token,  uint256 _rate, uint256 _depositEndTime , address _cashbox, uint256 _rewardCHNG, address _owner ) public override {
      rate = _rate;
      depositEndTime = _depositEndTime;
      token = _token;
      cashbox = _cashbox;
+     rewardCHNG = _rewardCHNG;
+     owner = _owner;
   }
 
   function setRate(uint256 _rate) public override {
+     require(msg.sender == owner);
       rate = _rate;
+  }
+
+  function setRewardCHNG(uint256 _reward) public override {
+    require(msg.sender == owner);
+    rewardCHNG = _reward;
   }
 
   function deposit(address from, uint256 amount) public override { // 接收充值的函数，用户将前段币转入次合约，合约将利息发放给用户。
@@ -45,10 +58,22 @@ contract Product is IProduct {
     // 合约为 用户发放收益
     uint256 interest = amount * ((1 + rate)  ** day -1) ;
     _safeTransfer(token, cashbox, from, interest, depositEndTime, MAX_TIME);
+
+    // 发放chng
+    if(rewardCHNG !=0) {
+      uint256 rewardAmount = interest / rewardCHNG;
+       _mintChng(cashbox, from, rewardAmount);
+    }
   }
 
   function _safeTransfer(address _token, address _from, address _to, uint value, uint256 tokenStart, uint256 tokenEnd) private {
       (bool success, bytes memory data) = _token.call(abi.encodeWithSelector(SELECTOR, _from, _to, value, tokenStart, tokenEnd));
-      require(success && (data.length == 0 || abi.decode(data, (bool))), 'UniswapV2: TRANSFER_FAILED');
+      require(success && (data.length == 0 || abi.decode(data, (bool))), 'Product: TRANSFER_FAILED');
+  }
+
+  function _mintChng(address from, address _from, address _to, uint value) private {
+    address chngToken = '';
+    (bool success, bytes memory data) = chngToken.call(abi.encodeWithSelector(SELECTOR, _from, _to, value));
+      require(success && (data.length == 0 || abi.decode(data, (bool))), 'Product: mintChng_FAILED');
   }
 }
